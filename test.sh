7691b7dc64ca21e05cd37fcd022f49e1c6991ec8
akhileshjoshi2 <akhileshjoshi2@gmail.com>
2023-07-11 12:46:51 +0530

    added path

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+uname -r
+pwd
+echo "Hello"
+echo 'forth commit'
+echo 'first commit on test4'
+
+echo 'second commit on test4'
+echo 'third commit on test4'
+echo 'first commit on test5'
+
