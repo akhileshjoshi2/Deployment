946723e7dd42bd3f9333977d29186feac2f49b1f
akhilesh.joshi@philips.com <akhilesh.joshi@philips.com>
2023-09-01 15:25:15 +0530

    merged from master

--- test.sh
+++ test.sh
@@ -3,4 +3,17 @@ uname -r
 pwd
 echo "Hello"
 echo 'forth commit'
+echo 'first commit on test4'
+
+echo 'second commit on test4'
+echo 'third commit on test4'
+echo 'first commit on test5'
+
+echo 'first commit to master'
+
+
+echo 'second commit to master'
+
+
+
 
