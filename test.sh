d0a1db44ba7cf3a1761bf9c8a4a785cbf701608b
akhilesh.joshi@philips.com <akhilesh.joshi@philips.com>
2023-09-01 15:25:15 +0530

    cicd changes

--- test.sh
+++ test.sh
@@ -3,4 +3,24 @@ uname -r
 pwd
 echo "Hello"
 echo 'forth commit'
+echo 'first commit on test4'
+
+echo 'second commit on test4'
+echo 'third commit on test4'
+echo 'first commit on test5'
+
+echo 'first commit to master'
+
+
+echo 'second commit to master'
+
+
+echo 'CI-CD changes'
+
+echo 'changed made to argocd'
+
+echo 'revertd changes'
+
+
+
 
